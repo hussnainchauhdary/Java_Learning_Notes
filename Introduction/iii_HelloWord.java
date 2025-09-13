f39a5147482b25a9e3fd17054ad15efdfdc11f13
Hussnain Ahmad <hussnainchauhdary210@gmail.com>
2025-09-13 17:39:47 +0500

    Delete Introduction directory

--- Introduction/iii_HelloWord.java
+++
@@ -1,9 +0,0 @@
-public class iii_HelloWord {
-    public static void main(String[] args) {
-
-        // program that print "Hello Word" on screen?
-
-        System.out.println("Hello Word");
-    }
-
-}
