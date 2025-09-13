a59fae0152b45aa329062a15a44a7a565c07c031
Hussnain Ahmad <hussnainchauhdary210.dev@gmail.com>
2025-09-13 18:23:59 +0500

    add introductionToJava

---
+++ a_IntroductionToJava/src/b_CommentsInJava.java
@@ -0,0 +1,15 @@
+public class b_CommentsInJava {
+    public static void main(String[] args) {
+
+        // This is a single-line comment
+        // Comments help explain code for humans.These are not part of code means never be executed.
+
+        /*
+         * This is a multi-line comment.
+
+         */
+
+        System.out.println("Java supports single-line and multi-line comments.");
+
+    }
+}
---
+++ a_IntroductionToJava/src/d_HelloWordPrint.java
@@ -0,0 +1,9 @@
+public class d_HelloWordPrint {
+
+    public static void main(String[] args) {
+
+        // program that print "Hello Word" on screen?
+
+        System.out.println("Hello Word");
+    }
+}
